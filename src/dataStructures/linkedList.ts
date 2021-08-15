768f077f5b17e581aa9a5595ae208d308a30aaaa
Barzilay, Oshri (ob143h) <ob143h@intl.att.com>
2021-08-16 02:55:12 +0300

    first code emited

--- src/dataStructures/linkedList.ts
+++ src/dataStructures/linkedList.ts
@@ -1,12 +1,42 @@
 // types
+import { produce }from 'immer';
+type ContentType  = string | number;
 
-interface Node{
-  next: Node;
+interface INode {
+  content: ContentType;
+  next: Node[];
   previous: Node[];
   add: (Node) => void;
   remove: (Node) => void;
 }
 
-function TestLinkedList(){
+class Node implements INode {
+  readonly content: ContentType;
+  readonly next: Node[];
+  readonly previous: Node[];
 
+  constructor(content: ContentType) {
+    this.content = content;
+  }
+
+  add: (Node) => void;
+  remove: (Node) => void;
+}
+
+export function TestLinkedListFromArray(data: ContentType[]) {
+  const list: Node = data.reduce<Node>((acc: Node | null, content) => {
+
+    const current = new Node(content);
+
+    if (acc === null) {
+      // new Node
+      acc = current;
+    } else {
+      acc.add(current);
+    }
+
+    return acc;
+  }, null);
+
+  return list;
 }
